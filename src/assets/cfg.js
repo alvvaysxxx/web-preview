7e9c07a7179062c94ea879e9ae4b21fcc45221b2
roshkoff571 <74923347+roshkoff571@users.noreply.github.com>
2023-08-24 16:33:06 +0300

    22

--- src/assets/cfg.js
+++ src/assets/cfg.js
@@ -1,4 +1,5 @@
 // А вы знали, что Amadeus хуила??
-const api = "http://localhost:3000";
+const api =
+  "https://jubilant-space-barnacle-6qxq9rgx4w7c56x5-3000.app.github.dev/";
 
 export default api;
